60b9d4ecde70b54c426c8905d5c84f0decd611d4
PanSTARRS <281744213@qq.com>
2023-03-08 19:49:14 +0800

    add test code

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import torch
 # import math
 import torch.nn.functional as F
 import bisect
+import numpy as np
+import cv2
 
 # a = torch.tensor([[2, 2], [3, 5], [9, 10]])
 # print(a.shape)
@@ -381,7 +383,7 @@ import bisect
 # for arr in sorted_boxes_lst:
 #     print(arr, "\n")
 
-# x, y = 0.5, 0.7
+# x, y = 0.5, 0.4
 # valid_boxes = set()
 # valid_boxes_subset = set()
 # result_1_range = range(bisect.bisect_left(sorted_boxes_lst[0][:, 0], x))
@@ -419,6 +421,106 @@ import bisect
 #     res &= result_i
 
 # print(res)
+# if len(res) > 1:
+#     print("Warning: the len of set is {}".format(len(res)))
 # for b in res:
 #     print(b)
 #     break
+
+
+from skimage.draw import bezier_curve
+import bezier
+import math
+
+img = np.zeros((512, 512, 3), np.uint8)
+rectangles = [
+    [60,60,80,80],
+    [123,78,147,94],
+    [168,70,188,92],
+    [90,75,110,101],
+    [378,405,398,427],
+    [360,310,380,330],
+    [390,335,410,361],
+    [400,356,424,372],
+    [398,380,418,402],
+]
+for rec in rectangles:
+    cv2.rectangle(img, rec[:2], rec[2:], (55,255,155), thickness=2)
+nodes1 = np.array([
+    [60, 95, 130, 188],
+    [65, 100, 90, 75],
+])
+curve1 = bezier.Curve(nodes1, degree=3)
+nodes2 = np.array([
+    [360, 420, 430, 377],
+    [308, 350, 390, 428],
+])
+curve2 = bezier.Curve(nodes2, degree=3)
+curves = [curve1, curve2]
+
+n = 30
+delta = 1/n
+for i in range(n+1):
+    for curve in curves:
+        point = curve.evaluate(i*delta)
+        x, y = round(point[0][0]), round(point[1][0])
+        cv2.circle(img, (x,y), 2, (255, 50, 0), 2)
+
+
+mapper = {0: 0, 1: 1}
+res_lst = [[0,3,1,2],[5,6,7,8,4]]
+points1 = torch.tensor(nodes1, dtype=torch.float32).transpose(0,1).reshape(1,8)
+points2 = torch.tensor(nodes2, dtype=torch.float32).transpose(0,1).reshape(1,8)
+points = torch.cat((points1, points2), dim=0)
+boxes = torch.tensor(rectangles, dtype=torch.float32)
+polygons = []
+
+def get_poly_one_curve(bezier_idx, n=5):
+    # TODO: 应该加一个 bezier_idx 到 self.res_lst 的每一项的 mapper
+    #       因为有些曲线并没有穿过 bbox 
+    assert bezier_idx in mapper
+    box_lst = res_lst[mapper[bezier_idx]]
+    bn = len(box_lst)
+    ref_num = max(2, bn//3+1)  # 每个顶点处的长度是平均了附近的 ref_num 个 box 的有关长度得来的
+    xs = points[bezier_idx][0::2].unsqueeze(0)  # [1, 4]
+    ys = points[bezier_idx][1::2].unsqueeze(0)
+    t = torch.linspace(0, 1, steps=n, dtype=xs.dtype, device=xs.device)  # [n,]
+    c0, c1, c2, c3 = (1-t)**3, 3 * (1-t)**2 * t, 3 * t**2 * (1-t), t**3
+    cs = torch.stack((c0, c1, c2, c3), dim=-1)  # [n, 4]
+    x, y = (cs*xs).sum(dim=1), (cs*ys).sum(dim=1)  # [n,]
+    # 切线系数
+    q0, q1, q2, q3 = -3*t**2 + 6*t - 3, 9*t**2 - 12*t + 3, -9*t**2 + 6*t, 3*t**2
+    qs = torch.stack((q0, q1, q2, q3), dim=-1)  # [n, 4]
+    dx, dy = (qs*xs).sum(dim=1), (qs*ys).sum(dim=1)  # [n,]
+    print(dx)
+    print(dy)
+    poly_vertex = torch.zeros(n*2, 2)
+    tmp_boxes = boxes[box_lst, :]
+    max_height = torch.max(tmp_boxes[:,3]-tmp_boxes[:,1])
+    max_width = torch.max(tmp_boxes[:,2]-tmp_boxes[:,0])
+    maxi = torch.max(max_height, max_width)
+    for i in range(n):
+        radius = maxi
+        # 先暂时这样干吧
+        rad = math.atan2(dx[i].item(), -dy[i].item())
+        _dx, _dy = radius/2 * math.cos(rad), radius/2 * math.sin(rad)
+        poly_vertex[i][0] = x[i] - _dx
+        poly_vertex[i][1] = y[i] - _dy
+        poly_vertex[n*2-i-1][0] = x[i] + _dx
+        poly_vertex[n*2-i-1][1] = y[i] + _dy
+    polygons.append(poly_vertex)
+
+n = 5
+get_poly_one_curve(0, n)
+get_poly_one_curve(1, n)
+for polygon in polygons:
+    for i in range(n*2):
+        xx, yy = round(polygon[i][0].item()), round(polygon[i][1].item())
+        # xx, yy = round(x[i].item()), round(y[i].item())
+        color = [round(255*(i/(n*2-1) * 0.6 + 0.4))] * 3
+        # print(color)
+        cv2.circle(img, (xx, yy), 2, color, 2)
+
+cv2.imshow("rectangle", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
